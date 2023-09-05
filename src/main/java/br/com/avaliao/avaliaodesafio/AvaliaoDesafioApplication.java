24f94c8aece6c7df31bd8975b4cbe8fdae2b2dc5
Alex-Victor <alexvictoribeiro@gmail.com>
2023-09-05 11:55:47 -0300

    init commit

---
+++ src/main/java/br/com/avaliao/avaliaodesafio/AvaliaoDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.avaliao.avaliaodesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliaoDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliaoDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/avaliao/avaliaodesafio/AvaliaoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avaliao.avaliaodesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliaoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
